603113f4c81de5d590ed205c5335f7cc2c9edb7e
ericong18 <ericong18@gmail.com>
2019-05-18 15:48:08 -0700

    Received from backend

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -5,7 +5,9 @@ export default class Results extends Component {
   render() {
     return (
       <div className="results-container">
-        
+        <div className="container-fluid">
+          <div className="row justify-content-center"></div>
+        </div>
       </div>
     )
   }
--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 import './Upload.css'
 import Dropzone from 'react-dropzone'
 import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios'
 import 'react-toastify/dist/ReactToastify.css';
 
 const uploadIcon = require('../assets/up-arrow.png')
+const url = `http://0e1f0eca.ngrok.io`
 
 export default class Upload extends Component {
   state = {
     file: undefined,
-    fileName: ""
+    fileName: "",
+    displayName: "",
+    score: undefined
   }
 
   handleFileDrop = acceptedFiles => {
@@ -19,11 +23,12 @@ export default class Upload extends Component {
     }
 
     let file = acceptedFiles[0]
-    if (file.type != "image/png") {
+    if (file.type != "image/jpeg") {
       toast.warn("Please upload a PNG file!")
       return
     }
     else {
+      console.log(acceptedFiles)
       this.setState({ 
         file,
         fileName: file.name
@@ -35,7 +40,27 @@ export default class Upload extends Component {
     e.preventDefault()
 
     if (this.state.file && this.state.fileName) {
-      // Send file to backend
+      let data = new FormData()
+      data.append('file', this.state.file)
+      data.append('fileName', this.state.fileName)
+
+      // fetch(`https://cors-anywhere.herokuapp.com/` + `${url}/foo`, {
+      //   method: 'POST',
+      //   body: data,
+      // }).then(res => {
+      //   // console.log(res.data)
+      //   return res.json()
+      // }).then(data => {
+      //   console.log(data)
+      // })
+
+      axios.post(`https://cors-anywhere.herokuapp.com/` + `${url}/foo`, data).then(res => {
+        this.setState({
+          displayName: res.data.displayName,
+          score: res.data.score
+        })
+      })
+
     }
   }
 
@@ -71,7 +96,7 @@ export default class Upload extends Component {
           { this.state.file && this.state.fileName && <p id="filename">{ this.state.fileName }</p> }
         </div>
         <div className="upload-button-container">
-          <button type="button" class="btn btn-primary">Upload</button>
+          <button type="button" class="btn btn-primary" onClick={ this.handleSubmit }>Upload</button>
         </div>
         <div className="toast-container">
           <div className="container-fluid">
