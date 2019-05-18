0ae57805a84983f67c4133494eee665124b6cadc
Joshua <jsun034@ucr.edu>
2019-05-18 16:59:50 -0700

    style results

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -6,6 +6,40 @@ export default class Results extends Component {
     return (
       <div className="results-container">
         
+      	<div className="container">
+		  <div className="row">
+		    
+
+		    <div className="col-8 dropzone-container leftCol">
+		      Left column
+		    </div>
+
+		    <div className="col-4 right-container rightCol">
+
+		    	<div className="right-container">
+			    
+			    <div className="row">
+			    	<div className="col dropzone-container topRight">
+			      		Top Right
+			    	</div>
+			    </div>
+			    <div className="row">
+			    	<div className="col dropzone-container botRight">
+			      		Bottom Right
+			        </div>
+			    </div>
+
+			    </div>
+		    </div>
+		  
+
+		  </div>
+		</div>
+
+
+
+
+
       </div>
     )
   }
