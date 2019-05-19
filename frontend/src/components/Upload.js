25a0305e51e17705b00ba1f4d5d616c93d3bc94e
Bryan Walsh <bryanwalsh00@gmail.com>
2019-05-18 18:28:16 -0700

    Add Yarn and Particles.js

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -3,8 +3,54 @@ import './Upload.css'
 import Dropzone from 'react-dropzone'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Particles from 'react-particles-js';
 
 const uploadIcon = require('../assets/up-arrow.png')
+
+const particlesOptions = {
+  particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      size: {
+          value: 5,
+          random: true,
+          anim: {
+              speed: 5,
+              size_min: 3
+          }
+      },
+      line_linked: {
+	            enable: false
+      },
+      move: {
+	            random: true,
+	            speed: 0.75,
+	            direction: "top",
+	            out_mode: "out"
+      },
+      color:  {
+        value: "#f66383"
+      },
+      interactivity: {
+	        events: {
+	            onclick: {
+	                enable: true,
+	                mode: "remove"
+	            }
+	        },
+	        modes: {
+	            remove: {
+	                particles_nb: 10
+	            }
+	        }
+	    }
+    }
+}
 
 export default class Upload extends Component {
   state = {
@@ -39,9 +85,11 @@ export default class Upload extends Component {
     }
   }
 
+
   render() {
     return (
       <div className="upload-container">
+      <Particles className='particles' params={particlesOptions}/>
         <div className="upload-desc-container">
           <div className="container-fluid">
             <div className="row justify-content-center">
