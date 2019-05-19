352eb26ce6c7d1287c5773da7597c090728a05c9
ericong18 <ericong18@gmail.com>
2019-05-18 17:00:40 -0700

    Received props from upload

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import './Results.css'
 
-export default class Results extends Component {
-  render() {
+const Results = props => {
+
+
     return (
       <div className="results-container">
         <div className="container-fluid">
-          <div className="row justify-content-center"></div>
+          <div className="row justify-content-center">
+            { props.location.state.score && <div>{props.location.state.score}</div> }
+          </div>
         </div>
       </div>
     )
-  }
 }
+
+export default Results
